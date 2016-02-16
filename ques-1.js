a8d8a04ffbc0c6f52792b4137da2c9f7844a57e1
SimplyY <SimplyYu@163.com>
2016-02-16 23:01:51 +0800

    fix ques-2， finish ques-1

--- ques-1.js
+++ ques-1.js
@@ -25,16 +25,13 @@ var injector = {
     },
     resolve: function(target) {
         // 步骤1：将函数target转化为字符串：
-        // TODO:
-        var fnText = ;
+        var fnText = target.toString();
         // 步骤2：健壮性起见，使用正则将函数的注释删除掉：
-        var STRIP_COMMENTS = ;
+        var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[^]*?\*\/))/mg;
         fnText = fnText.replace(STRIP_COMMENTS, '');
         // 步骤3：使用正则解析出函数的函数的依赖参数（字符串形式）：
-        // TODO:
-        var FN_ARGS = ;// 解析函数依赖参数正则
-        // TODO:
-        var argDecl = ;// 通过fnText.match()方法取得依赖参数的字符串数组
+        var FN_ARGS = /function\s*[^\(]*\(([^\)]*)\)/;// 解析函数依赖参数正则
+        var argDecl = fnText.match(FN_ARGS)[1].split(',');// 通过fnText.match()方法取得依赖参数的字符串数组
         // 步骤4：将真正依赖的变量存放到args数组中：
         var args = [];
 
@@ -47,12 +44,12 @@ var injector = {
         // 步骤5：返回待调用函数
         return function() {
             target.apply({}, args);
-        }
+        };
     }
 };
 
 injector.register('$scope', $scope);
-injector.register('$http', $http); 
+injector.register('$http', $http);
 
 TodoCtrl = injector.resolve(TodoCtrl);
 TodoCtrl();
--- ques-2.js
+++ ques-2.js
@@ -3,26 +3,24 @@
 // 1.请将数字字符串:'123'转化为数字：123：
 var str = '123';
 // SimplyY's code
-var int = Number.parseInt(str, 10);
+var int = parseInt(str, 10);
 console.log(int);
 
 // 2.使用Number函数将数字字符串数组：['1','2']转化为数字数组：[1,2]:
 var ary = ['1','2'];
 // SimplyY's code
-var intAry = ary.map(function(item) {
-    return Number(item);
-});
+var intAry = ary.map(Number);
 console.log(intAry);
 
 // 3.完成替换匹配正则
 // 考虑到了 win 下多种盘符的问题
-var regexp = /([C-Z]):\/[c-z](.*)/; // SimplyY's code
+var regexp = /^([A-Z]+:)(\/[^\/]+)/; 
 var path = 'D:/d/workbench/f2ehint-hook';
-path = path.replace(regexp, '$1$2');
+path = path.replace(regexp, '$1');
 console.log(path);
 
 // 测试：代码运行结果
 // communicate git:(master) ✗ node ques-2.js
 // 123
 // [ 1, 2 ]
-// D/workbench/f2ehint-hook
+// D:/workbench/f2ehint-hook
